7e4510b0d49b85357c3b484e4d90406268b2d2ac
kavinsky-spy <juliorapa@hotmail.com>
2023-12-24 18:42:28 -0300

    feat: Offer content entity

---
+++ web/modules/custom/offer/src/Entity/Offer.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\offer\Entity\Offer.
+ */
+
+namespace Drupal\offer\Entity;
+
+use Drupal\Core\Entity\EditorialContentEntityBase;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\EntityStorageInterface;
+
+/**
+ * Defines the offer entity.
+ *
+ * @ingroup offer
+ *
+ * @ContentEntityType(
+ *   id = "offer",
+ *   label = @Translation("Offer"),
+ *   base_table = "offer",
+ *   data_table = "offer_field_data",
+ *   revision_table = "offer_revision",
+ *   revision_data_table = "offer_field_revision",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "uuid" = "uuid",
+ *     "label" = "title",
+ *     "revision" = "vid",
+ *     "status" = "status",
+ *     "published" = "status",
+ *     "uid" = "uid",
+ *     "owner" = "uid",
+ *   },
+ *   revision_metadata_keys = {
+ *     "revision_user" = "revision_uid",
+ *     "revision_created" = "revision_timestamp",
+ *     "revision_log_message" = "revision_log"
+ *   },
+ *   handlers = {
+ *    "access" = "Drupal\offer\OfferAccessControlHandler",
+ *   }
+ * )
+ */
+
+class Offer extends EditorialContentEntityBase {
+
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+    $fields = parent::baseFieldDefinitions($entity_type); // provides id and uuid fields
+
+    $fields['user_id'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('User'))
+      ->setDescription(t('The user that created the offer.'))
+      ->setSetting('target_type', 'user')
+      ->setSetting('handler', 'default')
+      ->setDisplayOptions('view', [
+        'label' => 'hidden',
+        'type' => 'author',
+        'weight' => 0,
+      ])
+      ->setDisplayOptions('form', [
+        'type' => 'entity_reference_autocomplete',
+        'weight' => 5,
+        'settings' => [
+          'match_operator' => 'CONTAINS',
+          'size' => '60',
+          'autocomplete_type' => 'tags',
+          'placeholder' => '',
+        ],
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['title'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Title'))
+      ->setDescription(t('The title of the offer'))
+      ->setSettings([
+        'max_length' => 150,
+        'text_processing' => 0,
+      ])
+      ->setDefaultValue('')
+      ->setDisplayOptions('view', [
+        'label' => 'above',
+        'type' => 'string',
+        'weight' => -4,
+      ])
+      ->setDisplayOptions('form', [
+        'type' => 'string_textfield',
+        'weight' => -4,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['message'] = BaseFieldDefinition::create('string_long') // Create a new field definition for the 'message' field
+      ->setLabel(t('Message')) // Set the label for the field to 'Message'
+      ->setRequired(TRUE) // Set the field as required
+      ->setDisplayOptions('form', [ // Set the display options for the field in the form view
+          'type' => 'string_textarea', // Use a textarea for input
+          'weight' => 4, // Set the weight of the field in the form
+          'settings' => [
+            'rows' => 12, // Set the number of rows for the textarea
+          ],
+      ])
+      ->setDisplayConfigurable('form', TRUE) // Allow the field to be configurable in the form display settings
+      ->setDisplayOptions('view', [ // Set the display options for the field in the view mode
+          'type' => 'string', // Use a string formatter for display
+          'weight' => 0, // Set the weight of the field in the view
+          'label' => 'above', // Display the label above the field value
+      ])
+      ->setDisplayConfigurable('view', TRUE); // Allow the field to be configurable in the view display settings
+
+    $fields['status'] = BaseFieldDefinition::create('boolean')
+      ->setLabel(t('Publishing status'))
+      ->setDescription(t('A boolean indicating whether the Offer entity is published.'))
+      ->setDefaultValue(TRUE);
+
+    $fields['created'] = BaseFieldDefinition::create('created')
+      ->setLabel(t('Created'))
+      ->setDescription(t('The time that the entity was created.'));
+
+    $fields['changed'] = BaseFieldDefinition::create('changed')
+      ->setLabel(t('Changed'))
+      ->setDescription(t('The time that the entity was last edited.'));
+
+    return $fields;
+  }
+
+
+  /**
+   * {@inheritdoc}
+   * 
+   * Makes the current user the owner of the offer.
+   */
+  public static function preCreate(EntityStorageInterface $storage_controller, array &$values) {
+    parent::preCreate($storage_controller, $values);
+    $values += [
+      'user_id' => \Drupal::currentUser()->id(),
+    ];
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getOwner() {
+    return $this->get('user_id')->entity;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getOwnerId() {
+    return $this->get('user_id')->target_id;
+  }
+
+}
